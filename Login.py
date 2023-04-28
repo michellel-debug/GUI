8d46b8b79658f84b51daf5fadcbab0881557fd21
michellel-debug <legorretamichelle80@gmail.com>
2023-04-27 21:44:33 -0600

    Master

--- Login.py
+++ Login.py
@@ -4,8 +4,22 @@ from tkinter import ttk
 raiz = Tk()
 raiz.title("LogIn")
 
-marcoPrincipal = ttk.Frame(raiz)
+marcoPrincipal = ttk.Frame(raiz, padding= "20 20 20 20")
+marcoPrincipal.grid(column=0, row=0)
 
+txtUser = ttk.Entry(marcoPrincipal) 
+txtUser.grid(column=0, row=1)
 
+ttk.Label(marcoPrincipal, text="Username").grid(column=0, row=0)
+
+txtUser = ttk.Entry(marcoPrincipal) 
+txtUser.grid(column=0, row=3)
+
+ttk.Label(marcoPrincipal, text="Password").grid(column=0, row=2)
+
+ttk.Button(marcoPrincipal, text="Ingresar").grid(column=0, row=4)
+
+for hijo in marcoPrincipal.winfo_children():
+    hijo.grid_configure(padx=5, pady=5)
 
 raiz.mainloop()
