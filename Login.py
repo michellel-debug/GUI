c3d4dd15bee8d172d0b2c60c810739ee2af13ef9
michellel-debug <legorretamichelle80@gmail.com>
2023-04-27 22:15:03 -0600

    modified:   Login.py

--- Login.py
+++ Login.py
@@ -4,22 +4,24 @@ from tkinter import ttk
 raiz = Tk()
 raiz.title("LogIn")
 
-marcoPrincipal = ttk.Frame(raiz, padding= "20 20 20 20")
+marcoPrincipal = ttk.Frame(raiz, padding= "30 30 30 30")
 marcoPrincipal.grid(column=0, row=0)
 
-txtUser = ttk.Entry(marcoPrincipal) 
+txtUser = ttk.Entry(marcoPrincipal, width=15) #width define el tamaño de la text box
 txtUser.grid(column=0, row=1)
 
 ttk.Label(marcoPrincipal, text="Username").grid(column=0, row=0)
 
-txtUser = ttk.Entry(marcoPrincipal) 
-txtUser.grid(column=0, row=3)
+txtPassword = ttk.Entry(marcoPrincipal, width=15) 
+txtPassword.grid(column=0, row=3)
 
 ttk.Label(marcoPrincipal, text="Password").grid(column=0, row=2)
 
-ttk.Button(marcoPrincipal, text="Ingresar").grid(column=0, row=4)
+ttk.Button(marcoPrincipal, text="Log In").grid(column=0, row=4)
 
 for hijo in marcoPrincipal.winfo_children():
-    hijo.grid_configure(padx=5, pady=5)
+    hijo.grid_configure(padx=10, pady=10)
+
+txtUser.focus() #se usa cuando queremos que el cursor aparezca automáticamente
 
 raiz.mainloop()
